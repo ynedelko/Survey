6ec22ab297521d2344f0fbb66a9ae9a01411a93b
JuliannaGreen & YelenaNedelko <student@epicodus.com>
2015-08-25 10:08:27 -0700

    add spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,15 @@
 ENV ['RACK_ENV'] = 'test'
+
+require 'rspec'
+require 'pg'
+require 'sinatra/activerecord'
+require 'quiz'
+require 'question'
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Question.all().each() do |question|
+      question.destroy()
+    end
+  end
+end
