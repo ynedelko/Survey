3c926c8b7fc0f529f55fc56cd8e89a416c42122d
JuliannaGreen & YelenaNedelko <student@epicodus.com>
2015-08-25 11:53:37 -0700

    validate that a quiz has a name.

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -1,10 +1,13 @@
 class Quiz < ActiveRecord::Base
   has_many(:questions)
   before_save(:capitalize_name)
+  validates(:name, :presence => true)
 
 private
 
   define_method(:capitalize_name) do
     self.name=(name().capitalize())
   end
+
+
 end
