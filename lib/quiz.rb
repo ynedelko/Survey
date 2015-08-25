e1b12143a96a985f249a7ab4e71c0aa02b51e6e2
JuliannaGreen & YelenaNedelko <student@epicodus.com>
2015-08-25 10:48:25 -0700

    add the ability to capitalize quiz names

--- lib/quiz.rb
+++ lib/quiz.rb
@@ -1,3 +1,10 @@
 class Quiz < ActiveRecord::Base
   has_many(:questions)
+  before_save(:capitalize_name)
+
+private
+
+  define_method(:capitalize_name) do
+    self.name=(name().capitalize())
+  end
 end
